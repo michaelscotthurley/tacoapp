6777f0e92559887df1f09455c1f223683eeb5867
michaelscotthurley <michaelscotthurley>
2016-02-12 15:35:20 -0800

    Added description migration

---
+++ migrations/20160212152630-addDescriptionToTaco.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    queryInterface.addColumn(
+      'tacos',
+      'description',
+      Sequelize.TEXT
+    );
+
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    queryInterface.removeColumn(
+      'tacos',
+      'description',
+    );
+  }
+};
